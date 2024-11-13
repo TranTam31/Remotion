100ef47686e61ccbbd00913cdd3d0ad548c5a00e
TranTam31 <125586608+TranTam31@users.noreply.github.com>
2024-11-14 00:01:43 +0700

    feat: sign in with Google

--- app/src/main/java/com/example/hope/navigation/Destinations.kt
+++ app/src/main/java/com/example/hope/navigation/Destinations.kt
@@ -3,4 +3,5 @@ package com.example.hope.navigation
 object Destinations {
     const val NOTE_APP = "note_app"
     const val REMINDER_APP = "reminder_app"
+    const val PROFILE_APP = "profile_app"
 }
